798542613d4fb582a9994e4f79b1caf5cb41a4dc
Johan Hanses <johanhanses@gmail.com>
2020-02-17 14:04:24 +0100

    deployed dev and prod

--- url-shortener/redirect/index.js
+++ url-shortener/redirect/index.js
@@ -1,19 +1,46 @@
 "use strict";
 
+const AWS = require("aws-sdk");
+const tableName = process.env.DDB_TABLE;
+const docClient = new AWS.DynamoDB.DocumentClient();
+
 module.exports.handler = (event, context, callback) => {
     console.log(JSON.stringify(event));
 
     const slug = event.pathParameters.slug;
-    const target =
-        process.env["URL_" + slug.toUpperCase()] ||
-        "https://serverless.com/framework/docs/";
 
-    callback(null, {
-        statusCode: 302,
-        body: target,
-        headers: {
-            Location: target,
-            "Content-Type": "html/text"
+    docClient.get(
+        {
+            TableName: tableName,
+            Key: {
+                slug: slug
+            }
+        },
+        (err, data) => {
+            console.log(data);
+            if (err) {
+                console.log(err);
+                return callback(err);
+            }
+            if (data.Item) {
+                callback(null, {
+                    statusCode: 302,
+                    body: data.Item.long_url,
+                    headers: {
+                        Location: data.Item.long_url,
+                        "Content-Type": "text/plain"
+                    }
+                });
+            } else {
+                callback(null, {
+                    statusCode: 404,
+                    body:
+                        "This shortened link doesn't exist, make sure that you entered it right.",
+                    headers: {
+                        "Content-Type": "text/plain"
+                    }
+                });
+            }
         }
-    });
+    );
 };
